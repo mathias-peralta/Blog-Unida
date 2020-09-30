a917b7f4e99696bc99c9f7c28ae273a9b6418a40
Mathias Peralta <mathias.peralta@mail.com>
2020-09-30 14:30:48 -0400

    se agrego una nueva BD

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -1,9 +1,10 @@
 export default {
-    apiKey: "AIzaSyC5kMnre078afaR2T-3hQiSst22iJO48gA",
-    authDomain: "sabado-483e6.firebaseapp.com",
-    databaseURL: "https://sabado-483e6.firebaseio.com",
-    projectId: "sabado-483e6",
-    storageBucket: "sabado-483e6.appspot.com",
-    messagingSenderId: "267433685499",
-    appId: "1:267433685499:web:4d0fcbeae48db541455b5e"
+    apiKey: "AIzaSyDkMP16nnhR920Xvuxsje5UUJCkjLZLob4",
+    authDomain: "unida-blog-9ae91.firebaseapp.com",
+    databaseURL: "https://unida-blog-9ae91.firebaseio.com",
+    projectId: "unida-blog-9ae91",
+    storageBucket: "unida-blog-9ae91.appspot.com",
+    messagingSenderId: "486663063382",
+    appId: "1:486663063382:web:6cd99ef2482bc73d9038f7",
+    measurementId: "G-DMG57F8SY4"
 }
